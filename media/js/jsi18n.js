36ec5f533a2e1cae83ee6904c2a99bd11b7e6d58
Thomas Gruner <tom.gruner@gmail.com>
2012-07-16 15:38:24 +0200

    added back in legacy js files for /sqp/compare/items

---
+++ media/js/jsi18n.js
@@ -0,0 +1,14 @@
+/* gettext identity library */
+
+function gettext(msgid) { return msgid; }
+function ngettext(singular, plural, count) { return (count == 1) ? singular : plural; }
+function gettext_noop(msgid) { return msgid; }
+
+function interpolate(fmt, obj, named) {
+  if (named) {
+    return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])});
+  } else {
+    return fmt.replace(/%s/g, function(match){return String(obj.shift())});
+  }
+}
+
